5ce8978dfcdcf5946749c089a24ff490ef535e78
Abdur Rahim <abdoerrahiem@gmail.com>
2020-06-14 17:55:26 +0700

    added server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const mongoose = require('mongoose')
 const path = require('path')
 const { MONGOURI } = require('./keys')
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 mongoose.connect(MONGOURI, {
   useNewUrlParser: true,
