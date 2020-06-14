7389f13873a3567ab69451d9cdd65b6dc9d0ad1e
Abdur Rahim <abdoerrahiem@gmail.com>
2020-06-14 17:50:51 +0700

    added server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const path = require('path')
 const { MONGOURI } = require('./keys')
 const PORT = 5000
 
